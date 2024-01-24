b95ca136505cc5f5e8ec42ad1e917d9e08c84e78
MaelCap2 <mael.cap2vielle@gmail.com>
2024-01-24 08:22:56 +0100

    question 1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,31 +1,12 @@
 package personnages;
 
-public class Gaulois {
-	
-	private String nom;
-	private int force;
+public class Gaulois extends Personnage{
 	
 	public Gaulois(String n, int f) {
-		this.nom = n;
-		this.force = f;
+		super(n, f);
 	}
 	
-	public String getNom() {
-		return this.nom;
-	}
-	
-	public void parler(String t) {
-		System.out.println("Le Gaulois " + getNom() + " : << " + t + " >>.");
-	}
-	
-	public void frapper(Romain r) {
-		System.out.println(getNom() + " envoie un grand coup dans la mâchoire de " + r.getNom() + ".");
-		float f = this.force / 3;
-		r.recevoirCoup(f);
-	}
-	
-	public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("Astérix", 8);
-		System.out.println(asterix.getNom());
+	protected String donnerAuteur() {
+		return "Le  ";
 	}
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,33 +1,14 @@
 package personnages;
 
-public class Romain {
-	
-	private String nom;
-	private float force;
+public class Romain extends Personnage {
 	
 	public Romain(String n, int f) {
-		this.nom = n;
-		this.force = f;
+		super(n, f);
 	}
 	
-	public String getNom() {
-		return this.nom;
+	protected String donnerAuteur() {
+		return "Le romain ";
 	}
 	
-	public void parler(String t) {
-		System.out.println("Le Romain " + getNom() + " : << " + t + " >>.");
-	}
 	
-	public void recevoirCoup(float c) {
-		this.force = this.force - c;
-		if(this.force < 0) {
-			this.force = 0;
-		}
-		if(this.force > 0) {
-			parler("Aïe !");
-		} 
-		else {
-			parler("J'abandonne...");
-		}
-	}
 }
